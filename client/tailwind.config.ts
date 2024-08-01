ce9a0a881a3d1e8ced6ba9146cc709f1d0c799d9
nabeelktr <nabee446@gmail.com>
2024-08-01 15:39:49 +0530

    Add dockerfile

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,20 +1,37 @@
-import type { Config } from "tailwindcss";
+const withMT = require("@material-tailwind/react/utils/withMT");
 
-const config: Config = {
+module.exports = withMT({
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/utils/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/styles/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        Barlow: ["var(--font-Barlow)"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "custom-gradient": "linear-gradient(180deg, #3A3A3A 0%, #202020 100%)",
+      },
+      screens: {
+        "1000px": "1000px",
+        "1100px": "1100px",
+        "1200px": "1200px",
+        "1300px": "1300px",
+        "1500px": "1500px",
+        "800px": "800px",
+        "400px": "400px",
+      },
+      transitionDuration: {
+        '2000': '2000ms',
       },
     },
   },
   plugins: [],
-};
-export default config;
+});
