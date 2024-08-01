c6eb20e9f3c4491b263ec31c4719eac34ef12e83
nabeelktr <nabee446@gmail.com>
2024-08-01 11:41:59 +0530

    Add dto for project and todo

---
+++ backend/apps/project-service/src/dto/create-todo.request.ts
@@ -0,0 +1,35 @@
+import {
+  IsString,
+  IsOptional,
+  IsEnum,
+  IsNotEmpty,
+  IsDateString,
+} from "class-validator";
+import { TodoStatus } from "../enums/todo-status.enum";
+import { TodoPriority } from "../enums/todo-priority.enum";
+
+export class CreateTaskRequest {
+  @IsString()
+  @IsNotEmpty()
+  userId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsEnum(TodoStatus)
+  @IsOptional()
+  status: TodoStatus;
+
+  @IsEnum(TodoPriority)
+  @IsOptional()
+  priority: TodoPriority;
+
+  @IsDateString()
+  @IsOptional()
+  dueDate?: Date;
+}
